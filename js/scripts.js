e314540e21ad3044376bf66a0c1546a930ec12cb
Chris Breaux and Nate Wessels <chris@>
2019-01-17 10:44:37 -0800

    show and hide appropriate image and text when the radio button is selected

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,25 @@ $(document).ready(function() {
 
     var animal = $("input:radio[name=animal]:checked").val();
 
-    $("#random").text(animal);
-    $(".elephant").show();
+
+    if (animal === "Elephant") {
+      $(".tiger").hide();
+      $(".sloth").hide();
+      $(".elephant").show();
+    }
+
+    if (animal === "Tiger") {
+      $(".elephant").hide();
+      $(".sloth").hide();
+      $(".tiger").show();
+    }
+
+    if (animal === "Sloth") {
+      $(".elephant").hide();
+      $(".tiger").hide();
+      $(".sloth").show();
+    }
+
 
 
     event.preventDefault();
