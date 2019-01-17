8504146458fdf2ec14e70005e187083cdd6c360f
Chris Breaux and Nate Wessels <nate@>
2019-01-17 11:08:00 -0800

    toggle text display when user clicks on image, ensure previous text displays are not shown. test rebase -i

--- js/scripts.js
+++ js/scripts.js
@@ -3,32 +3,42 @@ $(document).ready(function() {
 
     var animal = $("input:radio[name=animal]:checked").val();
 
-
     if (animal === "Elephant") {
       $(".tiger").hide();
       $(".sloth").hide();
+      $("#tigerText").hide();
+      $("#slothText").hide();
       $(".elephant").show();
     }
 
     if (animal === "Tiger") {
       $(".elephant").hide();
       $(".sloth").hide();
+      $("#elephantText").hide();
+      $("#slothText").hide();
       $(".tiger").show();
     }
 
     if (animal === "Sloth") {
       $(".elephant").hide();
       $(".tiger").hide();
+      $("#tigerText").hide();
+      $("#elephantText").hide();
       $(".sloth").show();
     }
-
-
 
     event.preventDefault();
   });
 
+  $("img.elephant").click(function() {
+    $("#elephantText").toggle();
+  });
 
+  $("img.tiger").click(function() {
+    $("#tigerText").toggle();
+  });
 
-
-
+  $("img.sloth").click(function() {
+    $("#slothText").toggle();
+  });
 });
